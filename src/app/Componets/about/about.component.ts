3b006e7be3ed640cc52b2808ff8ee24471da28e1
sarautaher <128692610+sarautaher@users.noreply.github.com>
2024-06-18 00:51:13 +0300

    Update about.component.ts
    Sec

--- src/app/Componets/about/about.component.ts
+++ src/app/Componets/about/about.component.ts
@@ -8,7 +8,7 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit, AfterViewInit {
   about:any;
   Adout: any;
-  About_textt:any;
+  
  
 
   constructor() { }
@@ -19,7 +19,7 @@ export class AboutComponent implements OnInit, AfterViewInit {
     this.about= document.querySelector('.about');
     this.Adout = document.querySelector('#About');
   
-   this.About_textt=document.querySelector('.About_textt')  
+     
 
     window.addEventListener('scroll', this.onScroll.bind(this));
   }
@@ -30,11 +30,11 @@ export class AboutComponent implements OnInit, AfterViewInit {
 
     if (skillsPosition-10 < windowHeight) {
       this.about.classList.add('show');
-      this.About_textt.classList.add('show');
+      
  
     } else {
       this.about.classList.remove('show');
-      this.About_textt.classList.remove('show');
+      
     
     }
   }
