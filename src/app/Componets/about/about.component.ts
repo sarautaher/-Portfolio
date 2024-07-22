acd248d7c94f43336a2e97c98d187e9c0e9129ca
sarautaher <tahersara591@gmail.com>
2024-07-22 06:17:36 +0200

    sec commit

--- src/app/Componets/about/about.component.ts
+++ src/app/Componets/about/about.component.ts
@@ -1,41 +1,13 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import {Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent implements OnInit, AfterViewInit {
-  about:any;
-  Adout: any;
-  About_textt:any;
+export class AboutComponent  {
  
-
   constructor() { }
 
-  ngOnInit(): void { }
 
-  ngAfterViewInit(): void {
-    this.about= document.querySelector('.about');
-    this.Adout = document.querySelector('#About');
-  
-   this.About_textt=document.querySelector('.About_textt')  
-
-    window.addEventListener('scroll', this.onScroll.bind(this));
-  }
-
-  onScroll(): void {
-    let skillsPosition = this.Adout.getBoundingClientRect().top;
-    let windowHeight = window.innerHeight;
-
-    if (skillsPosition-10 < windowHeight) {
-      this.about.classList.add('show');
-      this.About_textt.classList.add('show');
- 
-    } else {
-      this.about.classList.remove('show');
-      this.About_textt.classList.remove('show');
-    
-    }
-  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as AOS from "aos";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app5';
+  ngOnInit(): void {
+    AOS.init()
+    window.addEventListener('scroll', AOS.refresh);  
+  }
 }
