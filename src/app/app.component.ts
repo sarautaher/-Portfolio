24917a170b4995190d8d094c5d9cfc8cda6c898f
sarautaher <128692610+sarautaher@users.noreply.github.com>
2024-07-22 21:36:17 +0300

    Update app.component.ts
    Sec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,14 @@ import * as AOS from "aos";
 export class AppComponent implements OnInit {
   title = 'app5';
   ngOnInit(): void {
-    AOS.init({
+   /* AOS.init({
       duration: 1200, 
       easing: 'ease-in-out', 
       once: false
     });
     window.addEventListener('scroll', AOS.refresh);
-   
+   */
+    AOS.init({disable: 'mobile'});
+    AOS.refresh();
   }
 }
